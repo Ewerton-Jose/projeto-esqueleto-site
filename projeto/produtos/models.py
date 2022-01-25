42146cd2eb72edf6e77de5a1c64d2f7afec0b4e1
Ewerton J. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2022-01-25 15:25:15 -0300

    banco de dados vinculado
    - models do produto foi criado (Agora tem o registro dos produtos)
    - Pequenas atualizações no css
    - Pequena atualizaçções no html
    - superAdmin criado

--- projeto/produtos/models.py
+++ projeto/produtos/models.py
@@ -1,3 +1,15 @@
+from pyexpat import model
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+
+    nomeP = models.CharField(max_length=255)
+    urlP = models.TextField()
+    descP = models.TextField()
+    precoP = models.FloatField()
+    qunati = models.IntegerField()
+    data_postagem = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nomeP
